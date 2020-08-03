874672dc98e10dde8b30197607542f18b7e086e5
bogdan <bogdan4888@gmail.com>
2020-08-03 19:56:47 +0300

    win

--- src/main/java/com/bogdan/Dao/UserDao.java
+++ src/main/java/com/bogdan/Dao/UserDao.java
@@ -11,65 +11,192 @@ import java.util.List;
 
 public class UserDao implements Storage {
 
+    private static Session session;
+
+    // Method 1: Create a new user in the database
     public void addUser(User user) {
-        //Create session factory object and getting session object from session factory
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(user);
-        transaction.commit();
-        session.close();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            System.out.println("\n>>> Successfully created user!\n");
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
     }
 
+    // Method 2: Update a user in the database
     public void updateUser(User user) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.update(user);
-        transaction.commit();
-        session.close();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.update(user);
+            transaction.commit();
+            System.out.println("\n>>> User with id = " + user.getId() + " is successfully updated!\n");
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
     }
 
+    // Method 3.1: Delete a user by id from the database
     public void removeUserById(int id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(getUserById(id));
-        transaction.commit();
-        session.close();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.delete(getUserById(id));
+            transaction.commit();
+            System.out.println("\n>>> Student with id = " + id + " is successfully deleted!\n");
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
     }
 
+    // Method 3.2: Delete a users by name from the database
     public void removeUserByName(String name) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(getUserByName(name));
-        transaction.commit();
-        session.close();
+        getUserByName(name);
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE FROM User WHERE name = :name").setParameter("name", name);
+            query.executeUpdate();
+            transaction.commit();
+            System.out.println("\n>>> Students with name = " + name + " is successfully deleted!\n");
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
     }
 
+    // Method 3.3: Delete all users from the database
     public void removeAll() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = String.format("DELETE FROM %s", User.class.getSimpleName());
-        Query query = session.createQuery(sql);
-        query.executeUpdate();
-        transaction.commit();
-        session.close();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE FROM User");
+            query.executeUpdate();
+            transaction.commit();
+            System.out.println("\n>>> Successfully deleted all users from the database!\n");
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
     }
 
+    // Method 4.1: Find user by id i the database
     public User getUserById(int id) {
-        return HibernateUtil.getSessionFactory().openSession().get(User.class, id);
+        User user = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            user = session.get(User.class, id);
+            transaction.commit();
+            System.out.println(">>> User with id = " + id + ":\n" + user);
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
+    // Method 4.2: Display all users by name from the database
     public List<User> getUserByName(String name) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        return session
-                .createQuery("FROM User WHERE name = :name", User.class)
-                .setParameter("name", name)
-                .list();
+        List<User> usersList = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            usersList = session.createQuery("FROM User WHERE name = :name", User.class).setParameter("name", name).list();
+            transaction.commit();
+            if (usersList != null) {
+                System.out.println(">>> User(s) with name = " + name + ": ");
+                for (User s : usersList) {
+                    System.out.println(s);
+                }
+            }
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+        return usersList;
     }
 
+    // Method 4.3: Display all users from the database
     public List<User> getAllUsers() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        transaction.commit();
-        return session.createQuery("FROM User", User.class).list();
+        List<User> usersList = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            usersList = session.createQuery("FROM User", User.class).list();
+            transaction.commit();
+            if (usersList != null) {
+                System.out.println(">>> All users:");
+                for (User s : usersList) {
+                    System.out.println(s);
+                }
+            }
+        } catch(Exception sqlException) {
+            if(session.getTransaction() != null) {
+                System.out.println("\nTransaction is being rolled back\n");
+                session.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
+        return usersList;
     }
 }
--- src/main/java/com/bogdan/Main.java
+++ src/main/java/com/bogdan/Main.java
@@ -8,37 +8,32 @@ public class Main {
     public static void main(String[] args) {
 
         UserDao userDao = new UserDao();
-        //userDao.removeAll();
-        //userDao.addUser(new User("Masha",26));
-        //userDao.addUser(new User("Masha",29));
-        //userDao.addUser(new User("Bob",24));
-        System.out.println(userDao.getAllUsers());
-        System.out.println(userDao.getUserByName("Mash"));
-        //userDao.removeUserById(30);
-        //System.out.println(userDao.getAllUsers());
 
-//        System.out.println("=================== CLEAR DATA BASE AND ADD NEW USERS =====================");
-//        userDao.removeAll();
-//        userDao.addUser(new User("Masha",26));
-//        userDao.addUser(new User("Bob",24));
-//        userDao.addUser(new User("Alex",18));
-//        userDao.addUser(new User("Viki",22));
-//        userDao.addUser(new User("Ana",27));
-//
-//        System.out.println("=================== GET USER BY ID, NAME AND ALL USERS =====================");
-//        System.out.println("User by id: " + userDao.getUserById(2));
-//        System.out.println("User by name: " + userDao.getUserByName("Masha"));
-//        System.out.println("All users: " + userDao.getAllUsers());
-//
-//        System.out.println("=================== CHANGE SOME INFO USER =====================");
-//        User user = userDao.getUserByName("Ana");
-//        System.out.println("User by name: " + user);
-//        user.setAge(37);
-//        userDao.updateUser(user);
-//        System.out.println("Updated user: " + userDao.getUserByName("Ana"));
-//
-//        System.out.println("=================== DELETE USERS BY ID AND NAME =====================");
-//        userDao.removeUserById(3);
-//        userDao.removeUserByName("Viki");
+        System.out.println("=================== CLEAR DATA BASE AND ADD NEW USERS =====================");
+        userDao.removeAll();
+        userDao.addUser(new User("Bob",24));
+        userDao.addUser(new User("Masha",26));
+        userDao.addUser(new User("Tommy",19));
+        userDao.addUser(new User("Alex",18));
+        userDao.addUser(new User("Viki",22));
+        userDao.addUser(new User("Ana",27));
+        userDao.addUser(new User("Alex",33));
+        userDao.addUser(new User("Poul",41));
+
+        System.out.println("=================== GET USER BY ID, NAME AND ALL USERS =====================");
+        userDao.getUserById(2);
+        userDao.getUserByName("Alex");
+        userDao.getAllUsers();
+
+        System.out.println("=================== CHANGE SOME INFO USER =====================");
+        User user = userDao.getUserById(2);
+        user.setAge(37);
+        user.setName("Moa");
+        userDao.updateUser(user);
+        userDao.getUserById(2);
+
+        System.out.println("=================== DELETE USERS BY ID AND NAME =====================");
+        userDao.removeUserById(2);
+        userDao.removeUserByName("Alex");
     }
 }
